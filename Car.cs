8a8fe079dfe7f3549e0b2372842ac2771ce32fc7
eliza1h <114587429+eliza1h@users.noreply.github.com>
2024-01-16 23:24:40 +0200

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_obiecte
+{
+    public class Car : Vehicle
+    {
+        private string color;
+        private string name;
+        private int maxSpeed;
+        private int year;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                if (value < 1980)
+                {
+                    throw new Exception(" Please pass a correct value ");
+                }
+                else
+                {
+                    year = value;
+                }
+            }
+        }
+        public Car(string nameName, string colorName, int yearValue, int maxSpeedValue)
+        {
+            name = nameName;
+            color = colorName;
+            year = yearValue;
+            maxSpeed = maxSpeedValue;
+        }
+
+        public Car(string nameName, string colorName, int yearValue, int maxSpeedValue, string eng_type)
+        {
+            name = nameName;
+            color = colorName;
+            year = yearValue;
+            maxSpeed = maxSpeedValue;
+            engine_type = eng_type;
+        }
+        public Car(string nameName)
+        {
+            name = nameName;
+        }
+        public void fullThrottle()
+        {
+            Console.WriteLine(name + " is going at the speed of " + maxSpeed);
+        }
+    }
+}
